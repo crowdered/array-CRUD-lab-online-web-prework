8cca5eb2ad9f114af8dc3cd2dbd6e1a810da32fa
Mark Crowder <crowdered@gmail.com>
2019-03-02 20:25:50 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -16,7 +16,7 @@ end
 
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
-  element = arrays.pop
+  element = array.pop
 end
 
 def remove_element_from_start_of_array(array)
